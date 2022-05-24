d57b00d334e876f2826d06e16da636423e166339
Mathew Adegbite <mathewadegbite@gmail.com>
2022-05-24 19:28:33 +0100

    modifying the api.js
    modifying the api.js

--- src/api.js
+++ src/api.js
@@ -1,4 +1,5 @@
-
+//export const  token = (new URLSearchParams(window.location.search)).get("jwt")
+//export const url = '/api/'
 
 export const url =  'http://localhost:8282/api/';
 export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjUzNDI5OTE4fQ.C60mMBJu3aOiXDGeZwcljxCWHAwmkz7HzoqhFA570D1SHdfJHnuuFrvzlZ1bmozuqgYEnd13NS_MO3Axj2hFAQ';
