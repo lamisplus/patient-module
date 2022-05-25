6b5f22612df05e646cdb15c7b74e0b2f27e67264
aniwange33 <terteseamos@gmail.com>
2022-05-25 06:06:28 +0100

    feat: make post endpoint to accept multiple services

--- src/main/java/org/lamisplus/modules/patient/controller/EncounterController.java
+++ src/main/java/org/lamisplus/modules/patient/controller/EncounterController.java
@@ -2,7 +2,8 @@ package org.lamisplus.modules.patient.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.lamisplus.modules.patient.domain.dto.EncounterDto;
+import org.lamisplus.modules.patient.domain.dto.EncounterRequestDto;
+import org.lamisplus.modules.patient.domain.dto.EncounterResponseDto;
 import org.lamisplus.modules.patient.service.EncounterService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,30 +19,30 @@ public class EncounterController {
     private final EncounterService encounterService;
 
     @PostMapping("/post")
-    public ResponseEntity<EncounterDto> createEncounter(@RequestBody EncounterDto encounterDto) {
-        return ResponseEntity.ok (encounterService.registerEncounter (encounterDto));
+    public ResponseEntity<List<EncounterResponseDto>> createEncounter(@RequestBody EncounterRequestDto encounterRequestDto) {
+        return ResponseEntity.ok (encounterService.registerEncounter (encounterRequestDto));
     }
 
     @GetMapping("encounter")
-    public ResponseEntity<List<EncounterDto>> getAllEncounter() {
+    public ResponseEntity<List<EncounterResponseDto>> getAllEncounter() {
         return ResponseEntity.ok (encounterService.getAllEncounters ());
     }
 
     @GetMapping("encounter/{id}")
-    public ResponseEntity<EncounterDto> getEncounter(@PathVariable("id") Long id) {
+    public ResponseEntity<EncounterResponseDto> getEncounter(@PathVariable("id") Long id) {
         return ResponseEntity.ok (encounterService.getEncounterById (id));
     }
 
     @GetMapping("encounter/person/{personId}")
-    public ResponseEntity<List<EncounterDto>> getPersonEncounter(@PathVariable("personId") Long personId) {
+    public ResponseEntity<List<EncounterResponseDto>> getPersonEncounter(@PathVariable("personId") Long personId) {
         return ResponseEntity.ok (encounterService.getAllEncounterByPerson (personId));
     }
 
     @PutMapping(value = "encounter/{id}")
-    public ResponseEntity<EncounterDto> updateEncounter(
+    public ResponseEntity<List<EncounterResponseDto>> updateEncounter(
             @PathVariable("id") Long id,
-            @RequestBody EncounterDto encounterDto) {
-        return ResponseEntity.ok (encounterService.updateEncounter (id, encounterDto));
+            @RequestBody EncounterRequestDto encounterRequestDto) {
+        return ResponseEntity.ok (encounterService.updateEncounter (id, encounterRequestDto));
     }
 
     @DeleteMapping(value = "encounter/{id}")
---
+++ src/main/java/org/lamisplus/modules/patient/domain/dto/EncounterResponseDto.java
@@ -0,0 +1,25 @@
+package org.lamisplus.modules.patient.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.PastOrPresent;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+public class EncounterResponseDto {
+    private Long facilityId;
+    private Long id;
+    @PastOrPresent
+    private LocalDate encounterDate;
+    private Long personId;
+    private String uuid;
+    private Long visitId;
+    private String serviceCode;
+    private String status;
+}
