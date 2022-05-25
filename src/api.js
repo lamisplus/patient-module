6f70625dca896e4f52c9ccf68d386fb7cbcba449
Mathew Adegbite <mathewadegbite@gmail.com>
2022-05-25 04:16:04 +0100

    update patient checkin

--- src/api.js
+++ src/api.js
@@ -1,5 +1,4 @@
-//export const  token = (new URLSearchParams(window.location.search)).get("jwt")
-//export const url = '/api/'
-
-export const url =  'http://localhost:8282/api/';
-export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjUzNDg5NzU4fQ.oR79CR6Ee4uTALF00Qd-KRT1nsSFuf4mg9x9AxR_KaBz5sinVfBpgLomsbBNZ_rEZ3hl1C4nOyIcDohUpMjP4g';
+export const  token = (new URLSearchParams(window.location.search)).get("jwt")
+export const url = '/api/'
+// export const url =  'http://localhost:8282/api/';
+// export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjUzNDg5NzU4fQ.oR79CR6Ee4uTALF00Qd-KRT1nsSFuf4mg9x9AxR_KaBz5sinVfBpgLomsbBNZ_rEZ3hl1C4nOyIcDohUpMjP4g';
