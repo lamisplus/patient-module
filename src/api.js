fcb17d25fccc140f4528adc1139721bdbd2379f9
Mathew Adegbite <mathewadegbite@gmail.com>
2022-05-25 09:41:24 +0100

    updating api.js
    updating api.js

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-// export const  token = (new URLSearchParams(window.location.search)).get("jwt")
-// export const url = '/api/'
-export const url =  'http://localhost:8282/api/';
-export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjUzNTA4ODY4fQ.b-pubmBvYRYDn5w-q7By00jx32mfGKgGaUE38jxxy7xAYDi1Fi3_arSkiH7eukHAy_Z5YsLK54tlkhZd2E8uqw'
+export const  token = (new URLSearchParams(window.location.search)).get("jwt")
+export const url = '/api/'
+// export const url =  'http://localhost:8282/api/';
+// export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjUzNTA4ODY4fQ.b-pubmBvYRYDn5w-q7By00jx32mfGKgGaUE38jxxy7xAYDi1Fi3_arSkiH7eukHAy_Z5YsLK54tlkhZd2E8uqw'
