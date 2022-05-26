3ac1a4fbcd48dcf8aac52076b9a09f89799a02d8
Mathew Adegbite <mathewadegbite@gmail.com>
2022-05-26 11:55:59 +0100

    updated code
    updated code

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
-// export const  token = (new URLSearchParams(window.location.search)).get("jwt")
-// export const url = '/api/'
+export const  token = (new URLSearchParams(window.location.search)).get("jwt")
+export const url = '/api/'
 
-export const url =  'http://localhost:8282/api/';
-export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjUzNTk0MjEyfQ.RDcL_5EmDtfjG7utFnRSHicymPCmf5o2dsIelkzzQsmNxZl_rkbi2cjLf6Ireh5-YE9A3h4BI61jkv3zavOzLA'
+// export const url =  'http://localhost:8282/api/';
+// export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjUzNTk0MjEyfQ.RDcL_5EmDtfjG7utFnRSHicymPCmf5o2dsIelkzzQsmNxZl_rkbi2cjLf6Ireh5-YE9A3h4BI61jkv3zavOzLA'
