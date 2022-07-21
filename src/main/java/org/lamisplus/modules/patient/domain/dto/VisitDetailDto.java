decdbe334c5459971d6a67f434369a9b8dbf6960
aniwange33 <terteseamos@gmail.com>
2022-07-21 12:31:51 +0100

    refactor: fix all pending issues on visit and encounter

--- src/main/java/org/lamisplus/modules/patient/domain/dto/VisitDetailDto.java
+++ src/main/java/org/lamisplus/modules/patient/domain/dto/VisitDetailDto.java
@@ -1,14 +1,11 @@
 package org.lamisplus.modules.patient.domain.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.Builder;
 import lombok.Data;
-import org.lamisplus.modules.patient.utility.LocalDateConverter;
 
-import javax.persistence.Convert;
-import javax.validation.constraints.PastOrPresent;
 import java.io.Serializable;
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Data
 @Builder
@@ -17,14 +14,8 @@ public class VisitDetailDto implements Serializable {
     private final Long id;
     private final Long personId;
     private final Long encounterId;
-    @PastOrPresent
-    @Convert(converter = LocalDateConverter.class)
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
-    private final LocalDate checkInDate;
-    @PastOrPresent
-    @Convert(converter = LocalDateConverter.class)
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
-    private final LocalDate checkOutDate;
+    private final LocalDateTime checkInDate;
+    private final LocalDateTime checkOutDate;
     private final String service;
     private final String status;
 }
