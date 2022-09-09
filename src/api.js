d6f0b43f896b2b8596a5ff8e6ee0fa440bb958f5
aniwange33 <terteseamos@gmail.com>
2022-09-09 18:09:21 +0100

    fix: visit can save with patient facilityID

--- src/api.js
+++ src/api.js
@@ -1,11 +1,11 @@
-/*
+
 export const  token = (new URLSearchParams(window.location.search)).get("jwt")
 export const url = '/api/v1/'
-*/
 
 
 
-export const url =  'http://localhost:8282/api/v1/';
-export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjYxMzYzMTYwfQ.cgCqAPdZV9YcC49sHU7Gsw-7CKG_1fAi5lFXStMAYN7Sv4pYuB4fMC9xb05SmPoK3ptjcfufucSFx8b7t3_tng'
+
+// export const url =  'http://localhost:8282/api/v1/';
+// export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjYxMzYzMTYwfQ.cgCqAPdZV9YcC49sHU7Gsw-7CKG_1fAi5lFXStMAYN7Sv4pYuB4fMC9xb05SmPoK3ptjcfufucSFx8b7t3_tng'
 
 
