c00c82bd0f0a5c81bc321ce3dc5d506d3606843a
gichangi <gichangijohn3@gmail.com>
2022-09-15 14:44:54 +0300

    Merge remote-tracking branch 'origin/gichangi' into gichangi

--- src/main/java/org/lamisplus/modules/patient/domain/dto/PersonResponseDto.java
+++ src/main/java/org/lamisplus/modules/patient/domain/dto/PersonResponseDto.java
@@ -41,6 +41,8 @@ public class PersonResponseDto {
     private Object address;
     private LocalDate dateOfRegistration;
     private Boolean IsDateOfBirthEstimated;
+    private LocalDateTime encounterDate;
+    private Long encounterId;
     private List<EncounterPersonListDto> encounters;
 
 
