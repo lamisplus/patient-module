5f56b092456c9c0e694400b20585f2ba90975009
gichangi <gichangijohn3@gmail.com>
2022-09-16 13:29:34 +0300

    checkedin service api bug resolved

--- src/api.js
+++ src/api.js
@@ -1,17 +1,12 @@
-/*
 export const  token = (new URLSearchParams(window.location.search)).get("jwt")
 export const url = '/api/v1/'
-*/
 
 
-<<<<<<<<< Temporary merge branch 1
+
 /*
 export const url =  'http://localhost:8282/api/v1/';
 export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjYyNjcwNTczfQ.Gc1Q7H4IylwXeUlsLnC_IRhayPaQvNtsw52mpLL1ZBTTgplr7zIzLJAry164H79fdmmmK64RGuC5ysqaMS859g';
-=========
 
-export const url =  'http://localhost:8282/api/v1/';
-export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjYxMzYzMTYwfQ.cgCqAPdZV9YcC49sHU7Gsw-7CKG_1fAi5lFXStMAYN7Sv4pYuB4fMC9xb05SmPoK3ptjcfufucSFx8b7t3_tng'
->>>>>>>>> Temporary merge branch 2
+*/
 
 
