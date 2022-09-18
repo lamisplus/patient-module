900eafe0ba2b7406b12c1acfe6f4c908db248736
gichangi <gichangijohn3@gmail.com>
2022-09-18 13:32:26 +0300

    git fixed

--- src/api.js
+++ src/api.js
@@ -1,12 +1,11 @@
-export const  token = (new URLSearchParams(window.location.search)).get("jwt")
-export const url = '/api/v1/'
+//export const  token = (new URLSearchParams(window.location.search)).get("jwt")
+//export const url = '/api/v1/'
 
 
 
-/*
+
 export const url =  'http://localhost:8282/api/v1/';
-export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjYyNjcwNTczfQ.Gc1Q7H4IylwXeUlsLnC_IRhayPaQvNtsw52mpLL1ZBTTgplr7zIzLJAry164H79fdmmmK64RGuC5ysqaMS859g';
+export const  token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjYzNTQwMjU3fQ.OiHxvbOXaFciAD72VpF61JxwXSCD9yHxFlZ73c3v_sN8taK6bsqUnGJlxGtRS0PpthWpkAIAxOTIRxF_KbCNng"
 
-*/
 
 
