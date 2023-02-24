3388bdf898948506e498405b667762be83d083cc
vixxjy <ajorvictor48@gmail.com>
2023-02-24 10:43:17 +0100

    [Patient] NIN updated

--- src/api.js
+++ src/api.js
@@ -1,11 +1,11 @@
-/*
+
 export const  token = (new URLSearchParams(window.location.search)).get("jwt")
 export const url = '/api/v1/'
-*/
 
+/*
 export const url =  'http://localhost:8282/api/v1/';
 export const  token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjc3MjY0NTA1fQ._VIFtjnC7WYHgRdLei7iIkym3_gtihUFCKGoTLFt8-AETBWbMjHmYxFX1IaarGSO9k8xJKd2ZUPl30D2Ipte1w";
-
+*/
 
 
 
