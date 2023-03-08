215db9ee8d09a3cdd26e731f604e497b33cc451a
vixxjy <ajorvictor48@gmail.com>
2023-03-08 10:55:54 +0100

    [Patient] Biometrics save button update

--- src/api.js
+++ src/api.js
@@ -3,8 +3,8 @@ export const  token = (new URLSearchParams(window.location.search)).get("jwt")
 export const url = '/api/v1/'
 
 /*
-export const url =  'http://localhost:8282/api/v1/';
-export const  token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjc3MjY0NTA1fQ._VIFtjnC7WYHgRdLei7iIkym3_gtihUFCKGoTLFt8-AETBWbMjHmYxFX1IaarGSO9k8xJKd2ZUPl30D2Ipte1w";
+export const url =  'http://localhost:8283/api/v1/';
+export const  token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjc4MzA4MTE5fQ.r3qkCYJ7sKvoYbc-emqcMl0RbwEvtOWiia_n9rJf_rle6RA9ytcnDcId3bqMyTDMWZAJoCOUlSkiv2Z-8X1Z3g";
 */
 
 
