c9a5434072352060a376a3d94a0831cb03bb19ca
vixxjy <ajorvictor48@gmail.com>
2023-03-14 12:05:06 +0100

    [Patient] updates for bug fix

--- src/api.js
+++ src/api.js
@@ -4,7 +4,7 @@ export const url = '/api/v1/'
 
 /*
 export const url =  'http://localhost:8283/api/v1/';
-export const  token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjc4NDI1NTQ4fQ.ZCQJVgMpsYaxuYxzmU0_XejDYeAB0myLX6_Ll2tgywvHJ3tpU8oDiW9kdtUsgjWlXpP8N4cc0GJxcrzGTl8jPw";
+export const  token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjc4NjE0ODQwfQ.sa1MzPh6IJC71Zg3BhuRTaW_pQjFJ0RM8A6HIBSHnbs7bS8NtW1BcAq0XpUGNm-7ns9Lsk2WwdtL8TjU7HOpwQ";
 */
 
 
