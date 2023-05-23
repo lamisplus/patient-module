38fcb7bf8a8d9df7033edfe3d9d6996ce6f037fa
vixxjy <ajorvictor48@gmail.com>
2023-05-23 14:16:13 +0100

    [patient] capture button added.

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
+/*
 export const token = new URLSearchParams(window.location.search).get("jwt");
 export const url = "/api/v1/";
+  */
 
-/*
 export const url = "http://localhost:9090/api/v1/";
 export const token =
-  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjg0NDU3MzYyfQ.26DtTpt8MGwimOIpLCw0zXDokV6G1w6xD8cdCMbsUVJZMk4lns76HrwC2Dl0VXVgdu-p05u6G3diVIcIzcfSfg";
-  */
+  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjg0Nzk1Mjg1fQ.esJM45I7w28jKU5ZHPMOVchN3ZkHl4RYM7V9ECIEVEajdulXLQ1MLS9BV74A0q7nanv7ZMGqkuHH-M8bFaNwrQ";
