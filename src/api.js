dbb9a58ccc70445a858c2bb8c18efe5ff0a2f182
Mathew Adegbite <mathewadegbite@gmail.com>
2023-06-18 22:30:56 +0100

    api update

--- src/api.js
+++ src/api.js
@@ -1,8 +1,9 @@
-
+/*
 export const token = new URLSearchParams(window.location.search).get("jwt");
 export const url = "/api/v1/";
-/*
-export const url = "http://lamisplus.org:7090/api/v1/";
+  */
+
+export const url = "http://localhost:8080/api/v1/";
 export const token =
-  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjkxNDQwNDY0fQ.yZhWhKoe_i0zGfNZpR30O6c8lb0PitwF6VQ2AR6aXlqP17SowFJkn9Tfx6we3zg_T5t0A2CiPaM-ACRuUhg0aQ";
-*/
+  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjg3MTY2ODIwfQ.kO_GhTjiJwkVpgg6SvQhoyVgkQKTO-K1GKcdlr6rAbpiPrWXLB8XaoqMkCeGjjNHDsAPbWTJX2yHpBW6PG7PgQ";
+
