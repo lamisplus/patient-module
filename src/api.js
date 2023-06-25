58dc2aab8db45bfbc45555ff1a161e21a7c6c98c
Mathew Adegbite <mathewadegbite@gmail.com>
2023-06-25 16:03:06 +0100

    capturing update

--- src/api.js
+++ src/api.js
@@ -1,9 +1,7 @@
-
-
 export const token = new URLSearchParams(window.location.search).get("jwt");
 export const url = "/api/v1/";
 /*
 export const url = "http://localhost:8080/api/v1/";
 export const token =
-  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjg3MzI4OTM4fQ.gk1J1jml8eyXBfS8JhfY8mF9AE1WUwsg36dIjCMwfpGsRx1U4PAcLq0yT-phdoaMfAM5Ey3Eo3K8LkLqMkbGcg";
+  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjg3NzQ1NzYxfQ._eiaT67wVdD-6fZBSy3b-ezpGo-jzFqOf7Osxx--3LNpvgEhQ7X8nkJG1LoVyuTWpMbAQnnUeoJdYE0yvm085w";
 */
