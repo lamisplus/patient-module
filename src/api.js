d4c2bed27b88b517a148ce0fae14b2063e87ed92
vixxjy <ajorvictor48@gmail.com>
2023-07-10 21:38:18 +0100

    [patient] fix

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@
 export const token = new URLSearchParams(window.location.search).get("jwt");
 export const url = "/api/v1/";
 /*
-export const url = "http://localhost:8382/api/v1/";
+export const url = "http://localhost:8383/api/v1/";
 export const token =
-  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjg4NzAxNDYzfQ.wHDcvoC8px7LvFUPbQ0kAHzRDfnrrf31twh1VxrC_dl7SiaUI7Gf418xK24s9az_oJZc0ZmYqOKNX3TRBMRKYw";
+  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjg5MDU4NzM5fQ.b6nY2YJSlEEqIXiJlAOMuVyrwUGmDIh7qK_ISOAn9Ud7CymziTatartlHkUtE2vrU8f3MzR1m8WpKNPqHwc0dg";
 */
