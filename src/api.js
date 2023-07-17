762435fa6ae6fb9b38d61fa34ca6a8fd9bdea7f5
vixxjy <ajorvictor48@gmail.com>
2023-07-17 22:15:10 +0100

    baseline recapture numbering added

--- src/api.js
+++ src/api.js
@@ -1,8 +1,7 @@
-
 export const token = new URLSearchParams(window.location.search).get("jwt");
 export const url = "/api/v1/";
 /*
 export const url = "http://localhost:8383/api/v1/";
 export const token =
-  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjg5MTk0MDQxfQ.p--pBMUzv-s66CMYB7-ohr7ekeXqTIdiG_lcDVw6nJm485H_KvcexyF5LqdJ2dIgCMvjHvoU5ssU6TJyPAARoA";
+  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjg5NjcwOTcwfQ.5PfolLOf498LNBiqZyFonAA9BZsOfKrTg5SjomaoOPYYQxLN5FWN7EY4A915cVBty92mm99qpA7PMvEyxYeAiA";
 */
