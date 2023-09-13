d5622944b00022889d46266aac50bf9318cf0eaa
vixxjy <ajorvictor48@gmail.com>
2023-09-13 09:16:09 +0100

    sprint update

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
+/*
 export const token = new URLSearchParams(window.location.search).get("jwt");
 export const url = "/api/v1/";
-
-/*
-export const url = "http://localhost:8080/api/v1/";
-export const token =
-  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjk0NDYyMTg4fQ.yBkl9hNKkXsV8pUDvVAA8Sdhf9EHPsp7F35d87yP_YIi3xMCI_mYd6UNIUzcAY7k1tRJE8FC_vTdbTE0asXsjg";
 */
+
+export const url = "http://localhost:8383/api/v1/";
+export const token =
+  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjk0NjM2MDIwfQ.o081fgVH-03-MDeCgtesCEecWz2uGHxszlyjMrpsk2UVGcgLeXG8C4sKiuPudXk9Q3sVc6VUrX21QlBWYc6SDQ";
