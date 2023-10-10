d5813ab7f4e7bfac3eabf2874b4b98ba90b85965
vixxjy <ajorvictor48@gmail.com>
2023-10-10 13:48:05 +0100

    [patient] added the check in unique patients

--- src/api.js
+++ src/api.js
@@ -4,5 +4,5 @@ export const url = "/api/v1/";
 /*
 export const url = "http://localhost:8383/api/v1/";
 export const token =
-  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjk0NjM2MDIwfQ.o081fgVH-03-MDeCgtesCEecWz2uGHxszlyjMrpsk2UVGcgLeXG8C4sKiuPudXk9Q3sVc6VUrX21QlBWYc6SDQ";
+  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjk2OTgwNzc5fQ.QCTzzkOF-XYI83-egjZF0e50haIanfQvbdyNrWWhpB_uGe8Q2-XGhbbWi3Mzwr2Uw35uI4r2ohmGYhHRWW3hwg";
 */
