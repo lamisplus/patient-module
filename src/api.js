28c59d4f26a7e875b202cc92a5b7006b19f749b5
vixxjy <ajorvictor48@gmail.com>
2023-10-10 13:48:05 +0100

    [patient] added the check in unique patients

--- src/api.js
+++ src/api.js
@@ -1,8 +1,7 @@
 export const token = new URLSearchParams(window.location.search).get("jwt");
 export const url = "/api/v1/";
-
 /*
 export const url = "http://localhost:8383/api/v1/";
 export const token =
-  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjk0NjM2MDIwfQ.o081fgVH-03-MDeCgtesCEecWz2uGHxszlyjMrpsk2UVGcgLeXG8C4sKiuPudXk9Q3sVc6VUrX21QlBWYc6SDQ";
+  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjk3MjI1OTE0fQ.JaSu0_85pXmlHReFkRcFSZdINGkZdEPYT1gkfbWw9k4mn4JUwwHeAoanhZvlppP6e4kLthQ2-6PzHYvhs-mtOw";
 */
--- src/main/java/org/lamisplus/modules/patient/repository/PersonRepository.java
+++ src/main/java/org/lamisplus/modules/patient/repository/PersonRepository.java
@@ -139,7 +139,7 @@ public interface PersonRepository extends JpaRepository<Person, Long> {
     @Query(value = "SELECT * FROM patient_person pp INNER JOIN patient_visit pv ON pp.uuid=pv.person_uuid WHERE (first_name ilike ?1 OR surname ilike ?1 OR other_name ilike ?1 OR full_name ilike ?1 OR hospital_number ilike ?1) AND pv.archived=?2 AND pp.archived=?2 AND pp.facility_id=?3 AND pv.visit_end_date is null", nativeQuery = true)
     Page<Person> findCheckedInPersonBySearchParameters(String queryParam, Integer archived, Long facilityId, Pageable pageable);
 
-    @Query(value = "SELECT * FROM patient_person pp INNER JOIN patient_visit pv ON pp.uuid=pv.person_uuid WHERE pv.archived=?1 AND pp.archived=?1 AND pp.facility_id=?2 AND pv.visit_end_date is null", nativeQuery = true)
+    @Query(value = "SELECT DISTINCT pp.* FROM patient_person pp INNER JOIN patient_visit pv ON pp.uuid=pv.person_uuid WHERE pv.archived=?1 AND pp.archived=?1 AND pp.facility_id=?2 AND pv.visit_end_date is null", nativeQuery = true)
     Page<Person> findAllCheckedInPerson(Integer archived, Long facilityId, Pageable pageable);
 
     @Query(
