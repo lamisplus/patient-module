d61c802cb1721e174ed7f38984fd8fe69504bbdb
vixxjy <ajorvictor48@gmail.com>
2024-01-10 21:48:43 +0100

    [patient] recapture no match fix to save after capture

--- src/api.js
+++ src/api.js
@@ -1,9 +1,8 @@
-
 export const token = new URLSearchParams(window.location.search).get("jwt");
 export const url = "/api/v1/";
 
 /*
 export const url = "http://localhost:8080/api/v1/";
 export const token =
-  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjk5NDgwOTI1fQ.sIIYvNf3jT0DkA46BYPDDHUN545-c3-f3zMP7ohKK0GvoBssKoX8SK6--fCsrJj3iqKQibOGRnr646m5X-dTMg";
+  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzA0OTU1MjU2fQ.FKIB2XBdBm3Nm3ngeGVSWJWD_wpKERi5ys_5Yebfm2fHOcEVkLpWW53WXimBQS95JCaTrqrUqa4gGzup3US1nw";
 */
