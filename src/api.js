4c198e49a552d7745f6ce02353f0bdf15e40f2e6
vixxjy <ajorvictor48@gmail.com>
2024-01-19 17:14:21 +0100

    [patient] updates file

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
+/*
 export const token = new URLSearchParams(window.location.search).get("jwt");
 export const url = "/api/v1/";
+*/
 
-/*
 export const url = "http://localhost:8080/api/v1/";
 export const token =
-  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzA1NjYyMDA0fQ.NA5Qst_mvWz_nFBWxnu8tp623Gpj5xQYJrbEpx1q2vbJDjuvnJEeQ57wMQpcnYMFoe59MGOQrX2uxZsR_O9jGg";
-*/
+  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzA1NzExMDg1fQ.JEQ-iSAyQW32_NRxlgrdF8-gTCzJNKePqty1vawUhCcx3OUxYmfxD7lzAgfRUqZoeAhDjJ89WzNp2DNi82Tmdw";
