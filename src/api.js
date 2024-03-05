7dcd492d75689c8ebf48fd5a327efad5568b6a13
vixxjy <ajorvictor48@gmail.com>
2024-03-05 20:05:43 +0100

    [patient] github link added

--- src/api.js
+++ src/api.js
@@ -4,5 +4,5 @@ export const url = "/api/v1/";
 /*
 export const url = "http://localhost:8383/api/v1/";
 export const token =
-  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzA3Nzk4NjExfQ.stQJThlJhPY6LXwXxh5uRFMjyvz0DHH522bXwPRRN1qk3ZUHcJr3El1lnNDIjk8wV8W5LCl_IJf1p2K9ws8etA";
+  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzA4Mzc2NzE2fQ.lYSa-6hNTQk5wLZcU2r-BQv6lDHtpZ9X27D6HA62xr_sUdYwC7nSL671q7TIKf4F-fhSp6ssue5yWkKkHPqNbw";
 */
