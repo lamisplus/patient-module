c01237c6bc93ef295b0fd0385555f353bc02c7c3
joshuagabriel-datafi <joshua.gabriel@thepalladiumgroup.com>
2025-01-13 15:56:52 +0100

    added a method to accomadate checks with serviceCode

--- src/main/java/org/lamisplus/modules/patient/repository/VisitRepository.java
+++ src/main/java/org/lamisplus/modules/patient/repository/VisitRepository.java
@@ -6,17 +6,21 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface VisitRepository extends JpaRepository<Visit, Long> {
     List<Visit> findAllByArchivedAndFacilityId(Integer archived, Long facilityId);
 
     List<Visit> getAllByVisitStartDateNotNullAndVisitEndDateIsNull();
 
     Optional<Visit> findVisitByPersonAndVisitStartDateNotNullAndVisitEndDateIsNull(Person person);
+
+    Optional<Visit> findVisitByPersonAndVisitStartDateNotNullAndVisitEndDateIsNullAndServiceCode (Person person, String serviceCode);
 
    /// Visit findByPersonAndStatus(Person person);
 
